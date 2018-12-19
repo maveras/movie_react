8db55408baf0db492ee4239ecfa9cbec532d6f84
Marcelo Vera Salvatierra <marcelo.verasalvatierra@gmail.com>
2018-12-18 23:35:16 -0300

    added detail movie css, and list movie information

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -3,7 +3,6 @@ import classes from './Movie.css'
 import { Link } from 'react-router-dom'
 const movie = props => (
   <div className="movie-card">
-    
     <div className="img">
       <img
         src={`https://image.tmdb.org/t/p/w500/${props.poster_path}`}
--- src/containers/Detail/Detail.js
+++ src/containers/Detail/Detail.js
@@ -1,9 +1,62 @@
 import React, { Component } from 'react'
-
+import axios from 'axios'
+import classes from './Detail.css'
 class Detail extends Component {
+  state = {
+    movie: null,
+    loading: true
+  }
+  componentDidMount() {
+    let movieId = this.props.match.params.movieId
+    axios
+      .get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=9124fe005d007e543def06ff8917205d&language=en-US&page=1`)
+      .then(response => {
+        console.log(response)
+        this.setState({ movie: response.data, loading: false })
+        console.log(this.state)
+      })
+      .catch(error => {
+        this.setState({ error: true })
+      })
+  }
   render() {
+    let movie = null
+    if (this.state.movie) {
+      let generes = this.state.movie.genres.map(genre => (
+        <div className="movie-gender">{genre.name}</div>
+      ))
+      let overview = this.state.movie.overview
+      let rating = this.state.movie.vote_average
+      let title = this.state.movie.title
+      movie = <div className="movie_container">
+          <div>
+            <img className="img-main " src={`https://image.tmdb.org/t/p/w500/${this.state.movie.backdrop_path}`} />
+          </div>
+          <div className="movie_detail">
+            <div className="movie-detail__cover">
+              <img src={`https://image.tmdb.org/t/p/w500/${this.state.movie.poster_path}`} />
+            </div>
+            <div className="main_detail">
+              <div className="movie__title">
+                <h3>{title}</h3>
+              </div>
+              <div className="movie__genres">{generes}</div>
+              <div className="movie__review">
+                <span>{overview}</span>
+              </div>
+              <div className="movie_eval">
+                <div className="movie_eval__rating">
+                <span>Rating: {rating}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    }
     return (
-      <h1>Movieas Detail</h1>
+      <div>
+        {movie}
+      </div>
     )
   }
 }
